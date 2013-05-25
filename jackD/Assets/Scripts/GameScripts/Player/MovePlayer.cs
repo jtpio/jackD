f8a0d54b499b0b0ee4171bb92e05be7db23c43c2
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2013-05-25 02:06:10 +0200

    Switched to character controller + player prefab

--- jackD/Assets/Scripts/GameScripts/Player/MovePlayer.cs
+++ jackD/Assets/Scripts/GameScripts/Player/MovePlayer.cs
@@ -3,10 +3,14 @@ using System.Collections;
 
 public class MovePlayer : MonoBehaviour {
 	
+	protected float speed = 3.0f;
+	
 	void Start () {
 	}
 	
 	void Update () {
-		transform.Translate(0.4f,0,0);
+		//transform.Translate(0.4f,0,0);
+		CharacterController controller = GetComponent<CharacterController>();
+        controller.SimpleMove(transform.right * speed);
 	}
 }
