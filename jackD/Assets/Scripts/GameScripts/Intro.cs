f5424881c2d76be27fbf5fa63f37db02023b9984
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2013-05-26 13:43:24 +0200

    Music intro.

--- jackD/Assets/Scripts/GameScripts/Intro.cs
+++ jackD/Assets/Scripts/GameScripts/Intro.cs
@@ -3,17 +3,39 @@ using System.Collections;
 
 public class Intro : MonoBehaviour {
 	
+	public GUIStyle style = new GUIStyle();
 	public float speed = 1;
 	
+	protected float timer = 0;
+	protected bool showText = false;
+		
 	void Start () {
-	
+		audio.Play();
 	}
 	
 	void Update () {
-		if (transform.position.x >= 53) {
-			Application.LoadLevel(1);
-			return;
+		if (showText) {
+			timer += Time.deltaTime;
+			audio.volume = audio.volume - 0.004f;
+			if (audio.volume <= 0) audio.volume = 0;
+			if (timer >= 5) {
+				audio.Stop ();
+				Application.LoadLevel(1);
+			}
+		} else if (transform.position.x >= 53) {
+			showText = true;
+			timer = 0;
+		} else {
+			transform.Translate(speed * Time.deltaTime, 0, 0);	
 		}
-		transform.Translate(speed * Time.deltaTime, 0, 0);	
+	}
+	
+	void OnGUI() {
+		
+		if (showText) {
+			GUI.Box(new Rect(Screen.width/2-200, Screen.height/2, 400f,40f), 
+			"Collect as much as possible before the time runs out", style
+			);
+		}
 	}
 }
--- jackD/Assets/Scripts/GameScripts/Player/Boost.cs
+++ jackD/Assets/Scripts/GameScripts/Player/Boost.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class Boost : MonoBehaviour {
 	
-	public float BOOST = 600;
+	public float BOOST = 350;
 	public float BOOST_TIME = 2;
 	
-	public float SLOW = 400;
+	public float SLOW = 250;
 	public float SLOW_TIME = 2;
 	
 	protected MovePlayer movePlayer;
