14007deb67c5e063bb79d72ba2ceedc4f584bc2a
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2013-05-26 06:38:13 +0200

    Sleeping screen off.

---
+++ jackD/Assets/Scripts/GameScripts/GameStuff.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameStuff : MonoBehaviour {
+
+	void Start () {
+		 Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	}
+	
+	void Update () {
+	
+	}
+}
