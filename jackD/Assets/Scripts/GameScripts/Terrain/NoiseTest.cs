8105f67d1cce359b8b1518f757ef11f4f3d33560
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2013-06-02 22:09:56 +0200

    New test scenes for testing noise.

--- jackD/Assets/Scripts/GameScripts/Terrain/NoiseTest.cs
+++ jackD/Assets/Scripts/GameScripts/Terrain/NoiseTest.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using System.Collections;
 
-public class Noise : MonoBehaviour {
+public class NoiseTest : MonoBehaviour {
 	
 	Terrain terrain;
-	public int offset = 5;
+	public int offset = 128;
+	public float t = 100f;
 	
 	void Start () {
+		GenerateNoise(20);
 	}
 	
 	public void GenerateNoise(int tileSize) {
@@ -15,14 +17,16 @@ public class Noise : MonoBehaviour {
 		for (int i = 0; i < terrain.terrainData.heightmapWidth; i++) {
 			for (int j = 0; j < terrain.terrainData.heightmapHeight; j++) {
 				heights[i,j] = 
-					Mathf.PerlinNoise(
-						(float)(i)/(float)terrain.terrainData.heightmapWidth * tileSize, 
-						(float)(j)/(float)terrain.terrainData.heightmapHeight * tileSize
-					) / 3.0f;
+					0.50f +
+					0.25f * Mathf.Sin(4 * Mathf.PI * i + 4 * t) * Mathf.Sin(2 * Mathf.PI * j + t) +
+					0.10f * Mathf.Cos(3 * Mathf.PI * i + 5 * t) * Mathf.Cos(5 * Mathf.PI * j + 3 * t) +
+					0.15f * Mathf.Sin(Mathf.PI * i + 0.6f * t);
 			}
 		}
-		terrain.terrainData.SetHeights(0,0, heights);	
+		terrain.terrainData.SetHeights(0, 0, heights);	
 	}
+	
+	
 	
 	void Update () {
 
